e9216d51544f4cdd1579feb9085f3d8819a4d8d2
sogaiu <983021772@users.noreply.github.com>
2019-10-02 11:16:32 +0900

    Add path-join

--- src/alc/index_defs/fs.clj
+++ src/alc/index_defs/fs.clj
@@ -5,3 +5,11 @@
   (if (not (.exists dir))
     (.mkdirs dir)
     true))
+
+(defn path-join
+  [root-path item-path]
+  (let [nio-path (java.nio.file.Paths/get root-path
+                   (into-array String []))]
+    (-> nio-path
+      (.resolve item-path)
+      .toString)))
