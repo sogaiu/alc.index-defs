4b6f4a09599d2bf141aa544b24b23ac8ad9ab002
sogaiu <983021772@users.noreply.github.com>
2019-11-06 05:22:07 +0900

    Improve comment in anaylze

--- src/alc/index_defs/analyze.clj
+++ src/alc/index_defs/analyze.clj
@@ -125,61 +125,69 @@
 
 ;; load-analysis is supposed to read in something like the following
 (comment
-  
+
   ;; structure of analysis data:
   ;;
   ;;   https://github.com/borkdude/clj-kondo/tree/
   ;;     120dd79bf3982c580e7784a41795853b44c9e4b0/analysis#user-content-data
   ;;
+  ;;   :lang: if occurred in a .cljc file, the language in which it was
+  ;;   resolved: :clj or :cljs
+  ;;
   {:findings [] ; not usually empty in practice
    :summary {} ; not usually empty in practice
    :analysis
    {:namespace-definitions
+    ;; sometimes there's :lang, but only if in .cljc file
     [{:filename (str (System/getenv "HOME")
                   "/src/antoine/src/antoine/renderer.cljs")
       :row 1
       :col 1
-      :name antoine.renderer}
+      :name 'antoine.renderer}
      {:filename (str (System/getenv "HOME")
                   "/.m2/repository/com/wsscode/pathom/2.2.7/pathom-2.2.7.jar"
                   ":"
                   "com/wsscode/pathom/parser.cljc")
       :row 1
       :col 1
-      :name com.wsscode.pathom.parser
+      :name 'com.wsscode.pathom.parser
       :lang :clj}]
     :namespace-usages
+    ;; sometimes there's :lang, but only if in .cljc file
+    ;; sometimes there's :alias, but it is extra info about the
+    ;; namespace - the entry is not just about the alias
     [{:filename (str (System/getenv "HOME")
                   "/src/antoine/src/antoine/renderer.cljs")
-      :row 3
-      :col 5
-      :from antoine.renderer
-      :to clojure.core.async}]
+      :row 3 ; row of :to (e.g. of in [clojure.core.async] in require)
+      :col 5 ; col of :to (e.g. of in [clojure.core.async] in require)
+      :from 'antoine.renderer
+      :to 'clojure.core.async}]
     :var-definitions
+    ;; sometimes there's :lang, but only if in .cljc file
     [{:filename (str (System/getenv "HOME")
                   "/.m2/repository/com/wsscode/pathom/2.2.7/pathom-2.2.7.jar"
                   ":"
                   "com/wsscode/pathom/parser.cljc")
       :row 13
       :col 1
-      :ns com.wsscode.pathom.parser
-      :name expr->ast
+      :ns 'com.wsscode.pathom.parser
+      :name 'expr->ast
       :lang :clj}]
     :var-usages
+    ;; sometimes there's :lang, but only if in .cljc file
     [{:name defmacro
-      :var-args-min-arity 2
-      :lang :cljs
+      :var-args-min-arity 2 ; optional
+      :lang :cljs  ; optional
       :filename (str (System/getenv "HOME")
                   "/.m2/repository/org/clojure/clojurescript/1.10.520/"
                   "clojurescript-1.10.520.jar"
                   ":"
                   "cljs/spec/gen/alpha.cljc")
-      :from cljs.spec.gen.alpha
-      :macro true
+      :from 'cljs.spec.gen.alpha
+      :macro true ; optional
       :col 1
-      :arity 4
+      :arity 4 ; optional
       :row 62
-      :to cljs.core}]}}
-              
-  )
+      :to 'cljs.core}]}} ; can be empty (was for :name js/isNaN, js/parseInt)
 
+  )
