9936818f6603f84a172051154170cce3d5e0298d
sogaiu <983021772@users.noreply.github.com>
2020-01-22 00:20:31 +0000

    Search PATH for binary

--- src/alc/index_defs/impl/paths.clj
+++ src/alc/index_defs/impl/paths.clj
@@ -4,56 +4,89 @@
    [clojure.java.shell :as cjs]
    [clojure.string :as cs]))
 
+(defn which
+  [bin-name]
+  ;; from taylorwood
+  (let [paths
+        (cs/split (System/getenv "PATH")
+          (re-pattern (java.io.File/pathSeparator)))]
+    (first
+      (for [path (distinct paths)
+            :let [file (cji/file path bin-name)]
+            :when (.exists file)]
+        (.getAbsolutePath file)))))
+
 (defmulti get-lint-paths
   (fn [method proj-root opts]
     method))
 
 (defmethod get-lint-paths :boot
   [_ proj-root {:keys [:verbose]}]
-  (let [{:keys [:err :exit :out]} (cjs/with-sh-dir proj-root
-                                    (cjs/sh "boot" "with-cp" "-w" "-f" "-"))]
+  (let [boot-path (which "boot")
+        _ (assert boot-path
+            "failed to find boot")
+        {:keys [:err :exit :out]}
+        (cjs/with-sh-dir proj-root
+          (cjs/sh boot-path "with-cp" "-w" "-f" "-"))]
     (assert (= 0 exit)
       (str "`boot with-cp -w -f` failed to determine classpath\n"
         "  exit\n" exit "\n"
         "  out:\n" out "\n"
-        "  err:\n" err "\n"))
+        "  err:\n" err "\n"
+        "  boot-path:\n" boot-path "\n"))
     (cs/trim out)))
 
 ;; XXX: any benefit in using tools.deps directly?
 (defmethod get-lint-paths :clj
   [_ proj-root {:keys [:verbose]}]
-  (let [{:keys [:err :exit :out]} (cjs/with-sh-dir proj-root
-                                    (cjs/sh "clj" "-Spath"))]
+  (let [clj-path (which "clj")
+        _ (assert clj-path
+            "failed to find clj")
+        {:keys [:err :exit :out]}
+        (cjs/with-sh-dir proj-root
+          (cjs/sh clj-path "-Spath"))]
     (assert (= 0 exit)
       (str "`clj -Spath` failed to determine classpath\n"
         "  exit\n" exit "\n"
         "  out:\n" out "\n"
-        "  err:\n" err "\n"))
+        "  err:\n" err "\n"
+        "  clj-path:\n" clj-path "\n"))
     ;; out has a trailing newline because clj uses echo
     (cs/trim out)))
 
 (defmethod get-lint-paths :custom
   [_ proj-root {:keys [:cp-command :verbose]}]
-  (let [{:keys [:err :exit :out]} (cjs/with-sh-dir proj-root
-                                    (apply cjs/sh cp-command))]
+  (let [cmd (first cp-command)
+        custom-path (which cmd)
+        _ (assert custom-path
+            (str "failed to find " cmd))
+        {:keys [:err :exit :out]}
+        (cjs/with-sh-dir proj-root
+          (apply cjs/sh cp-command))]
     (assert (= 0 exit)
       (str "`"
         (cs/join " " cp-command)
         "` failed to determine classpath\n"
         "  exit\n" exit "\n"
         "  out:\n" out "\n"
-        "  err:\n" err "\n"))
+        "  err:\n" err "\n"
+        "  custom-path:\n" custom-path "\n"))
     (cs/trim out)))
 
 (defmethod get-lint-paths :lein
   [_ proj-root {:keys [:verbose]}]
-  (let [{:keys [:err :exit :out]} (cjs/with-sh-dir proj-root
-                                    (cjs/sh "lein" "classpath"))]
+  (let [lein-path (which "lein")
+        _ (assert lein-path
+            "failed to find lein")
+        {:keys [:err :exit :out]}
+        (cjs/with-sh-dir proj-root
+          (cjs/sh lein-path "classpath"))]
     (assert (= 0 exit)
       (str "`lein classpath` failed to determine classpath\n"
         "  exit\n" exit "\n"
         "  out:\n" out "\n"
-        "  err:\n" err "\n"))
+        "  err:\n" err "\n"
+        "  lein-path:\n" lein-path "\n"))
     (cs/trim out)))
 
 ;; XXX: yarn over npx -- provide way to force one?
@@ -86,10 +119,10 @@
                  (assert false
                    (str "No yarn.lock or package-lock.json in: " proj-root)))
         silent-flag (cond
-                      (= runner "yarn")
+                      yarn?
                       "--silent"
                       ;;
-                      (= runner "npx")
+                      npx?
                       "--quiet"
                       ;;
                       :else
@@ -97,10 +130,22 @@
                         (str "unexpected runner: " runner)))
         _ (when verbose
             (println (str "  chose " runner " to invoke shadow-cljs")))
+        runner-path (cond
+                      yarn?
+                      (which "yarn")
+                      ;;
+                      npx?
+                      (which "npx")
+                      ;;
+                      :else
+                      (assert false
+                        (str "*** unexpected condition ***")))
+        _ (assert runner-path
+            (str "failed to find " runner))
         ;; min version of shadow-cljs 2.8.53 for classpath ability
         {:keys [:err :exit :out]}
         (cjs/with-sh-dir proj-root
-          (cjs/sh runner silent-flag "shadow-cljs" "info"))
+          (cjs/sh runner-path silent-flag "shadow-cljs" "info"))
         [_ major minor patch]
         (re-find #"cli version: (\d+)\.(\d+)\.(\d+)" err)
         _ (assert (and (>= (Integer/parseInt major) 2)
@@ -110,11 +155,12 @@
                        major "." minor "." patch))
         {:keys [:err :exit :out]}
         (cjs/with-sh-dir proj-root
-          (cjs/sh runner silent-flag "shadow-cljs" "classpath"))]
+          (cjs/sh runner-path silent-flag "shadow-cljs" "classpath"))]
     (assert (= 0 exit)
       (str "`" runner " shadow-cljs classpath` "
         "failed to determine classpath\n"
         "  exit\n" exit "\n"
         "  out:\n" out "\n"
-        "  err:\n" err "\n"))
+        "  err:\n" err "\n"
+        "  runner-path:\n" runner-path "\n"))
     (cs/trim out)))
